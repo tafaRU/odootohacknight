6a89df4b830026e4694e9af2624c348b6d7064fc
Alex Comba <alex.comba@agilebg.com>
2017-03-30 12:42:07 +0200

    [ADD] todo_app as sample to start coding

---
+++ sample_code/todo_app/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Alex Comba - https://github.com/tafaRU
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Todo Application',
+    'summary': 'Todo app [Odoo Development Essentials Book]'
+               'used as base to start coding at odootohacknight',
+    'website': 'https://github.com/tafaRU/odootohacknight/wiki',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Daniel Reis',
+    'depends': [
+        'base'
+    ],
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'security/todo_access_rules.xml',
+    #     'views/todo_menu.xml',
+    #     'views/todo_view.xml'
+    # ],
+    'application': True
+ }
